4f4b9d059e77cf7c778a9b6f8c9d33476f7b15b2
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2021-05-18 07:48:42 +0200

    Add files via upload

---
+++ xml/xml/Klijent_class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xml
+{
+    class Klijent_class
+    {
+    }
+}
